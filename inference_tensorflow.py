6aa08cdceb5874d18b39b69d5be726be597f41b7
Yemin Shi <shiyemin2@qq.com>
2017-05-19 13:01:05 +0800

    add tensorflow benchmark support

---
+++ inference_tensorflow.py
@@ -0,0 +1,132 @@
+import os
+import sys
+#  import the deep learning framework you want to use
+#  ...
+import numpy as np
+import argparse
+import time
+import tensorflow as tf
+
+from models.tensorflow import nets_factory
+
+slim = tf.contrib.slim
+#  change DLLIB to the deep learning framework's name
+#  ...
+DLLIB = 'TensorFlow'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='test CNN inference speed')
+    parser.add_argument('--network', type=str, default='resnet50',
+                        choices=['alexnet', 'inception-v1', 'inception-v2', 'inception-v3',
+                                 'inception-v4', 'inception-resnet-v2', 'resnet50', 'resnet101', 'resnet152',
+                                 'resnet200', 'resnet_v2_50', 'resnet_v2_101', 'resnet_v2_152', 'resnet_v2_200',
+                                 'vgga', 'vgg16', 'vgg19'],
+                        help = 'network name')
+    parser.add_argument('--params', type=str, help='model parameters')
+    parser.add_argument('--batch-size', type=int, default=1, help='batch size')
+    parser.add_argument('--im-size', type=int, help='image size')
+    parser.add_argument('--n-sample', type=int, default=1000, help='number of samples')
+    parser.add_argument('--gpu', type=str, default='0', help='gpu device')
+    parser.add_argument('--n-epoch', type=int, default=10, help='number of epochs')
+    parser.add_argument('--verbose', type=lambda x: x.lower() in ("yes", 'true', 't', '1'), default=True,
+                        help='verbose information')
+    args = parser.parse_args()
+
+    network_fn = nets_factory.get_network_fn(args.network, num_classes=1001, weight_decay=0.00004, is_training=False)
+
+    print('===================== benchmark for %s %s =====================' % (DLLIB, args.network))
+    print('n_sample=%d, batch size=%d, num epoch=%d' %  (args.n_sample, args.batch_size, args.n_epoch))
+
+    im_size = 224
+    if args.im_size is not None:
+        im_size = args.im_size
+    elif hasattr(network_fn, 'default_image_size'):
+        im_size = network_fn.default_image_size
+
+    #  loading model
+    t1 = time.time()
+
+    #  set up the network model for inference task
+    #  ...
+    inputs = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, im_size, im_size, 3])
+    logits, end_points = network_fn(inputs)
+    config = tf.ConfigProto(allow_soft_placement=True,
+                            log_device_placement=False,
+                            gpu_options=tf.GPUOptions(allow_growth=True,
+                                                      visible_device_list=args.gpu))
+    sess = tf.InteractiveSession(config=config)
+
+    if args.params is not None:
+        #  load pre-trained weights
+        variables_to_restore = slim.get_model_variables()
+        restore_op = slim.assign_from_checkpoint_fn(args.params,
+                                       variables_to_restore)
+        restore_op(sess)
+        print('Load parameters from %s' % (args.params))
+    else:
+        #  initialize randomly
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+        print('Initialize randomly')
+
+    #  ...
+
+    t2 = time.time()
+    print('Finish loading model in %.4fs' % (t2-t1))
+
+    t1 = time.time()
+    data_list = [np.random.uniform(-1, 1, (args.batch_size, im_size, im_size, 3)).astype(np.float32)
+                 for i in range(int(np.ceil(1.0*args.n_sample/args.batch_size)))]
+
+    #  transform the data from numpy.ndarrays to the needed format(i.e. torch.Tensor)
+    #  ...
+
+    t2 = time.time()
+    print('Generate %d random images in %.4fs!' % (args.n_sample, t2-t1))
+
+    # Run 1 epoch so that TF allocate the gpu memory
+    for j, batch in enumerate(data_list[0:2]):
+        output = sess.run([logits], feed_dict={inputs:batch})
+
+    t_list = []
+    t_start = time.time()
+    for i in range(args.n_epoch):
+        t1 = time.time()
+
+        #  iterate and forward all data samples for one epoch
+        #  ...
+        for j, batch in enumerate(data_list):
+            output = sess.run([logits], feed_dict={inputs:batch})
+
+        t2 = time.time()
+        t_list.append(t2-t1)
+        if args.verbose:
+            print('Epoch %d, finish %d images in %.4fs, speed = %.4f image/s' % (i, args.n_sample, t2-t1, args.n_sample/(t2-t1)))
+
+    t_end = time.time()
+
+    t_list = np.array(t_list)
+    if args.n_epoch > 2:
+        #  delete the slowest and fastest results
+        argmax = t_list.argmax()
+        argmin = t_list.argmin()
+        t_list[argmax] = 0
+        t_list[argmin] = 0
+        t_avg = np.sum(t_list)/(args.n_epoch-2)
+    else:
+        t_avg = np.sum(t_list)/args.n_epoch
+    print('Finish %d images for %d times in %.4fs, speed = %.4f image/s (%.4f ms/image)' % (args.n_sample, args.n_epoch, t_end-t_start, args.n_sample/t_avg, t_avg*1000.0/args.n_sample))
+
+    print('===================== benchmark finished =====================')
+
+    from utils import get_gpu_memory
+    gpu_mem = get_gpu_memory()
+
+    #  save results
+    res_dir = 'cache/results'
+    if not os.path.exists(res_dir):
+        os.makedirs(res_dir)
+
+    res_file_path = os.path.join(res_dir, '%s_%s_%d.txt' % (DLLIB, args.network, args.batch_size))
+    with open(res_file_path, 'w') as fd:
+        fd.write('%s %s %d %f %d' % (DLLIB, args.network, args.batch_size, args.n_sample/t_avg, gpu_mem))
---
+++ models/tensorflow/nets_factory.py
@@ -0,0 +1,100 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Contains a factory for building various models."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import functools
+
+import tensorflow as tf
+
+from . import alexnet
+from . import inception
+from . import resnet_v1
+from . import resnet_v2
+from . import vgg
+
+slim = tf.contrib.slim
+
+networks_map = {'alexnet': alexnet.alexnet_v2,
+                'vgga': vgg.vgg_a,
+                'vgg16': vgg.vgg_16,
+                'vgg19': vgg.vgg_19,
+                'inception-v1': inception.inception_v1,
+                'inception-v2': inception.inception_v2,
+                'inception-v3': inception.inception_v3,
+                'inception-v4': inception.inception_v4,
+                'inception-resnet-v2': inception.inception_resnet_v2,
+                'resnet50': resnet_v1.resnet_v1_50,
+                'resnet101': resnet_v1.resnet_v1_101,
+                'resnet152': resnet_v1.resnet_v1_152,
+                'resnet200': resnet_v1.resnet_v1_200,
+                'resnet_v2_50': resnet_v2.resnet_v2_50,
+                'resnet_v2_101': resnet_v2.resnet_v2_101,
+                'resnet_v2_152': resnet_v2.resnet_v2_152,
+                'resnet_v2_200': resnet_v2.resnet_v2_200,
+               }
+
+arg_scopes_map = {'alexnet': alexnet.alexnet_v2_arg_scope,
+                  'vgga': vgg.vgg_arg_scope,
+                  'vgg16': vgg.vgg_arg_scope,
+                  'vgg19': vgg.vgg_arg_scope,
+                  'inception-v1': inception.inception_v3_arg_scope,
+                  'inception-v2': inception.inception_v3_arg_scope,
+                  'inception-v3': inception.inception_v3_arg_scope,
+                  'inception-v4': inception.inception_v4_arg_scope,
+                  'inception-resnet-v2':
+                  inception.inception_resnet_v2_arg_scope,
+                  'resnet50': resnet_v1.resnet_arg_scope,
+                  'resnet101': resnet_v1.resnet_arg_scope,
+                  'resnet152': resnet_v1.resnet_arg_scope,
+                  'resnet200': resnet_v1.resnet_arg_scope,
+                  'resnet_v2_50': resnet_v2.resnet_arg_scope,
+                  'resnet_v2_101': resnet_v2.resnet_arg_scope,
+                  'resnet_v2_152': resnet_v2.resnet_arg_scope,
+                  'resnet_v2_200': resnet_v2.resnet_arg_scope,
+                 }
+
+
+def get_network_fn(name, num_classes, weight_decay=0.0, is_training=False):
+  """Returns a network_fn such as `logits, end_points = network_fn(images)`.
+
+  Args:
+    name: The name of the network.
+    num_classes: The number of classes to use for classification.
+    weight_decay: The l2 coefficient for the model weights.
+    is_training: `True` if the model is being used for training and `False`
+      otherwise.
+
+  Returns:
+    network_fn: A function that applies the model to a batch of images. It has
+      the following signature:
+        logits, end_points = network_fn(images)
+  Raises:
+    ValueError: If network `name` is not recognized.
+  """
+  if name not in networks_map:
+    raise ValueError('Name of network unknown %s' % name)
+  func = networks_map[name]
+  @functools.wraps(func)
+  def network_fn(images):
+    arg_scope = arg_scopes_map[name](weight_decay=weight_decay)
+    with slim.arg_scope(arg_scope):
+      return func(images, num_classes, is_training=is_training)
+  if hasattr(func, 'default_image_size'):
+    network_fn.default_image_size = func.default_image_size
+
+  return network_fn
