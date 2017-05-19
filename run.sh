418c5fcd72eda3c8ca02454a453207d43b71b107
nicklhy <nicklhy@gmail.com>
2017-05-19 16:04:28 +0800

    add python variable

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+PYTHON=python
 NETWORK_LIST="alexnet vgg16 vgg19 inception-bn inception-v3 resnet50 resnet101 resnet152"
 GPU=0
 BATCH_SIZE_LIST="1 2 4 8 16 32 64 128"
@@ -13,7 +14,7 @@ do
     do
         for BATCH_SIZE in ${BATCH_SIZE_LIST}
         do
-            CUDA_VISIBLE_DEVICES=${GPU} python inference_${DLLIB}.py --network ${NETWORK} \
+            CUDA_VISIBLE_DEVICES=${GPU} ${PYTHON} inference_${DLLIB}.py --network ${NETWORK} \
                 --batch-size ${BATCH_SIZE} \
                 --n-sample 1000 \
                 --n-epoch ${N_EPOCH} \
@@ -24,5 +25,5 @@ done
 
 for NETWORK in ${NETWORK_LIST}
 do
-    python plot_speed.py --network ${NETWORK}
+    ${PYTHON} plot_speed.py --network ${NETWORK}
 done
